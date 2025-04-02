ba4e1f82edb145175b96f21099803a09757c403f
Aibek Serikbay <aibek@Aibeks-MacBook-Pro.local>
2025-04-02 06:49:22 -0400

    feat: make Header responsive and add mobile menu toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem.jsx";
 
 function Header() {
+    const [toggle, setToggle] = React.useState(false);
+
     const menu = [
         {
             name: "HOME",
@@ -37,9 +39,28 @@ function Header() {
         <div className='flex items-center justify-between p-5'>
             <div className='flex items-center gap-8'>
                 <img src={logo} alt="logo" className='w-[80px] md:w-[150px]' />
-                {menu.map((item) => (
-                    <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
-                ))}
+                <div className='hidden md:flex gap-8'>
+                    {menu.map((item) => (
+                        <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
+                <div className='md:hidden flex gap-5'>
+                    {menu.map((item, index) =>
+                        index < 3 ? (
+                        <HeaderItem key={item.name} name={''} Icon={item.icon} />
+                        ) : null
+                    )}
+                </div>
+                <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                    <HeaderItem name={''} Icon={<HiDotsVertical />} />
+                    {toggle ? <div className='absolute bg-[#121212] border-[1px] border-gray-500 p-3 mt-3'>
+                        {menu.map((item, index) =>
+                            index >= 3 ? (
+                                <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+                            ) : null
+                        )}
+                    </div> : null}
+                </div>
             </div>
             <div>
                 <img src={userAvatar} alt="userAvatar" className='w-[60px] rounded-full' />
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HeaderItem({name, Icon}) {
     return (
-        <div className="text-white flex items-center gap-3 text-xl font-semibold cursor-pointer hover:underline underline-offset-8">
+        <div className="text-white flex items-center gap-3 text-xl font-semibold cursor-pointer hover:underline underline-offset-8 mb-2">
             <h2>{Icon}</h2>
             <h2>{name}</h2>
         </div>
