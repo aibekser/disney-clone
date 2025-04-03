62ef3cee89898f07202bdfacdda42c0315cb7083
Aibek Serikbay <aibek@Aibeks-MacBook-Pro.local>
2025-04-03 05:31:57 -0400

    feat: add Axios API call setup (not yet used)

---
+++ src/services/GlobalApi.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
+const getTrendingMovies = axios.get(movieBaseUrl + "/trending/all/day?api_key=" + apiKey);
+
+export default {
+    getTrendingMovies,
+}
